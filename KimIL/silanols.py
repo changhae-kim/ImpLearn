09004c9418bfb454939173ae6064fe25a06a830a
Changhae Andrew Kim <changhae_kim@outlook.com>
2022-08-10 11:34:04 -0500

    keep track of OH groups when exporting

--- KimIL/silanols.py
+++ KimIL/silanols.py
@@ -38,32 +38,6 @@ class Silanols():
             slab.set_cell(pbc)
         return slab
 
-    def analyze_bonds (self, slab=None, bond_cutoffs=None):
-
-        if slab is None:
-            slab = self.slab
-        if bond_cutoffs is None:
-            bond_cutoffs = self.bond_cutoffs
-
-        atoms = slab.get_chemical_symbols()
-        bonds = neighbor_list('ij', slab, bond_cutoffs)
-
-        coord_numbers = {'Si': 4, 'O': 2, 'H': 1}
-        issues = []
-        for i, X in enumerate(atoms):
-            i_neighbors = bonds[1][bonds[0] == i]
-            if len(i_neighbors) != coord_numbers[X]:
-                issues.append([X, i, i_neighbors])
-
-        print('Bonding Analysis')
-        for X, i, i_neighbors in issues:
-            message = '{:s}{:d} is bonded to'.format(X, i+1)
-            for j in i_neighbors:
-                message += ' {:s}{:d} {:f}'.format(atoms[j], j+1, slab.get_distance(i, j, mic=True))
-            print(message)
-
-        return
-
     def find_OH_groups(self, slab=None, bond_cutoffs=None, exclude_waters=True):
 
         if slab is None:
@@ -103,12 +77,12 @@ class Silanols():
         geminal_OH_pairs = []
         for n, OH1_group in enumerate(OH_groups):
             for m, OH2_group in enumerate(OH_groups):
-                if n > m:
+                if n < m:
                     O1_neighbors = bonds[1][bonds[0] == OH1_group[1]]
                     O2_neighbors = bonds[1][bonds[0] == OH2_group[1]]
                     for i in numpy.intersect1d(O1_neighbors, O2_neighbors):
                         if atoms[i] == 'Si':
-                            geminal_OH_pairs.append([OH1_group, OH2_group])
+                            geminal_OH_pairs.append([n, m])
 
         return geminal_OH_pairs
 
@@ -127,7 +101,7 @@ class Silanols():
         vicinal_OH_pairs = []
         for n, OH1_group in enumerate(OH_groups):
             for m, OH2_group in enumerate(OH_groups):
-                if n > m:
+                if n < m:
                     O1_neighbors = bonds[1][bonds[0] == OH1_group[1]]
                     O2_neighbors = bonds[1][bonds[0] == OH2_group[1]]
                     geminal = False
@@ -142,7 +116,7 @@ class Silanols():
                     Si2_candidate_neighbors = [bonds[1][bonds[0] == i] for i in Si2_candidates]
                     for i in numpy.intersect1d(numpy.concatenate(Si1_candidate_neighbors), numpy.concatenate(Si2_candidate_neighbors)):
                         if atoms[i] == 'O':
-                            vicinal_OH_pairs.append([OH1_group, OH2_group])
+                            vicinal_OH_pairs.append([n, m])
 
         return vicinal_OH_pairs
 
@@ -160,26 +134,28 @@ class Silanols():
         viable_OH_pairs = []
         for n, OH1_group in enumerate(OH_groups):
             for m, OH2_group in enumerate(OH_groups):
-                if n > m:
+                if n < m:
                     if slab.get_distance(OH1_group[1], OH2_group[1], mic=True) < viable_cutoff:
                         if exclude_geminals:
                             geminal = False
-                            for (OH3_group, OH4_group) in geminal_OH_pairs:
-                                if OH1_group[1] in [OH3_group[1], OH4_group[1]] or OH2_group[1] in [OH3_group[1], OH4_group[1]]:
+                            for pair in geminal_OH_pairs:
+                                if n in pair or m in pair:
                                     geminal = True
                                     break
                             if geminal:
                                 continue
-                        viable_OH_pairs.append([OH1_group, OH2_group])
+                        viable_OH_pairs.append([n, m])
 
         return viable_OH_pairs
 
-    def carve_minimal_clusters(self, slab=None, bond_cutoffs=None, viable_OH_pairs=None, OH_bond_length=None, reorder_podals=True):
+    def carve_minimal_clusters(self, slab=None, bond_cutoffs=None, OH_groups=None, viable_OH_pairs=None, OH_bond_length=None, reorder_podals=True):
 
         if slab is None:
             slab = self.slab
         if bond_cutoffs is None:
             bond_cutoffs = self.bond_cutoffs
+        if OH_groups is None:
+            OH_groups = self.OH_groups
         if viable_OH_pairs is None:
             viable_OH_pairs = self.viable_OH_pairs
         if OH_bond_length is None:
@@ -189,7 +165,10 @@ class Silanols():
         bonds = neighbor_list('ij', slab, bond_cutoffs)
 
         minimal_clusters = []
-        for (OH1_group, OH2_group) in viable_OH_pairs:
+        for a, b in viable_OH_pairs:
+
+            OH1_group = OH_groups[a]
+            OH2_group = OH_groups[b]
 
             cluster_atoms = []
             cluster_coords = []
@@ -270,6 +249,32 @@ class Silanols():
 
         return minimal_clusters
 
+    def analyze_bonds (self, slab=None, bond_cutoffs=None):
+
+        if slab is None:
+            slab = self.slab
+        if bond_cutoffs is None:
+            bond_cutoffs = self.bond_cutoffs
+
+        atoms = slab.get_chemical_symbols()
+        bonds = neighbor_list('ij', slab, bond_cutoffs)
+
+        coord_numbers = {'Si': 4, 'O': 2, 'H': 1}
+        issues = []
+        for i, X in enumerate(atoms):
+            i_neighbors = bonds[1][bonds[0] == i]
+            if len(i_neighbors) != coord_numbers[X]:
+                issues.append([X, i, i_neighbors])
+
+        print('Bonding Analysis')
+        for X, i, i_neighbors in issues:
+            message = '{:s}{:d} is bonded to'.format(X, i+1)
+            for j in i_neighbors:
+                message += ' {:s}{:d} {:f}'.format(atoms[j], j+1, slab.get_distance(i, j, mic=True))
+            print(message)
+
+        return
+
     def analyze_distances(self, file_path, slab=None):
 
         if slab is None:
@@ -296,11 +301,13 @@ class Silanols():
 
         return
 
-    def save_clusters(self, file_path, file_type, minimal_clusters=None):
+    def save_clusters(self, file_path, file_type, viable_OH_pairs=None, minimal_clusters=None):
+        if viable_OH_pairs is None:
+            viable_OH_pairs = self.viable_OH_pairs
         if minimal_clusters is None:
             minimal_clusters = self.minimal_clusters
-        for i, cluster in enumerate(minimal_clusters):
-            write(file_path.format(i), cluster, file_type)
+        for (a, b), cluster in zip(viable_OH_pairs, minimal_clusters):
+            write(file_path.format(a, b), cluster, file_type)
         return
 
 
@@ -332,6 +339,6 @@ if __name__ == '__main__':
     print(clusters.viable_OH_pairs)
     clusters.analyze_bonds()
     clusters.analyze_distances('A_d{:s}{:s}.png')
-    clusters.save_clusters('A_{:04d}.xyz', 'xyz')
+    clusters.save_clusters('A_{:03d}_{:03d}.xyz', 'xyz')
 
 
