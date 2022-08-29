ff58886337c85fec054fd057837806cf088b89a8
Changhae Andrew Kim <changhae_kim@outlook.com>
2022-08-29 15:41:32 -0500

    pucker reintroduced

--- KimIL/phillips.py
+++ KimIL/phillips.py
@@ -44,17 +44,17 @@ class Phillips():
 
     def do_polymer(self, alkyl_lengths=[4, 6]):
 
-        self.L_butyl_cluster = self.attach_alkyl(alkyl_lengths[0], point_y=True)
+        self.L_butyl_cluster = self.attach_alkyl(alkyl_lengths[0], point_y=True, pucker=-1)
         self.L_butyl_R_ethylene_cluster = self.attach_ethylene(
-                cluster=self.attach_alkyl(alkyl_lengths[0], point_y=True, relax=False),
-                point_y=False, relax=True)
-        self.LR_transition_state_cluster = self.attach_transition_state(alkyl_lengths[1], point_y=False)
+                cluster=self.attach_alkyl(alkyl_lengths[0], point_y=True, pucker=0, relax=False),
+                point_y=False, pucker=-1, relax=True)
+        self.LR_transition_state_cluster = self.attach_transition_state(alkyl_lengths[1], point_y=False, pucker=-1)
 
-        self.R_butyl_cluster = self.attach_alkyl(alkyl_lengths[0], point_y=False)
+        self.R_butyl_cluster = self.attach_alkyl(alkyl_lengths[0], point_y=False, pucker=+1)
         self.R_butyl_L_ethylene_cluster = self.attach_ethylene(
-                cluster=self.attach_alkyl(alkyl_lengths[0], point_y=False, relax=False),
-                point_y=True, relax=True)
-        self.RL_transition_state_cluster = self.attach_transition_state(alkyl_lengths[1], point_y=True)
+                cluster=self.attach_alkyl(alkyl_lengths[0], point_y=False, pucker=0, relax=False),
+                point_y=True, pucker=+1, relax=True)
+        self.RL_transition_state_cluster = self.attach_transition_state(alkyl_lengths[1], point_y=True, pucker=+1)
 
         self.done_polymer = True
 
