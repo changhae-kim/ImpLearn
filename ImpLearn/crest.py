6cedec9cc200608de6075cc50897625757497d3c
Changhae Andrew Kim <changhae_kim@outlook.com>
2023-03-08 13:48:21 -0600

    bug fixes

--- ImpLearn/crest.py
+++ ImpLearn/crest.py
@@ -41,6 +41,8 @@ class Crest():
 
         if isinstance(constraints, str):
             self.constraints = [constraints] * n_struct
+        else:
+            self.constraints = constraints
 
         return
 
@@ -52,27 +54,36 @@ class Crest():
 #$ -N crest-{label:s}
 #$ -pe default {n_proc:d}
 
-/home/cakim2/conda/envs/petersgroup/bin/crest coord --T {n_proc:d} --chrg {charge:d} --uhf {mult:d} --cinp constraints.inp --subrmsd >> {label:s}.log
+/home/cakim2/conda/envs/petersgroup/bin/crest coord --T {n_proc:d} --chrg {charge:d} --uhf {mult:d} --cinp constraints.inp --subrmsd --noreftopo >crest.log 2>&1
+
 '''
 
         n_struct = len(self.file_paths)
         for i in range(n_struct):
-            label = os.path.basename(self.file_paths[i]).rsplit('.', 1)[0]
+            file_name = os.path.basename(self.file_paths[i])
+            label = file_name.rsplit('.', 1)[0]
             work_dir = os.path.join(self.prefix, label)
-
             if not os.path.exists(work_dir):
-                os.system('mkdir {:s}'.format(work_dir))
-                os.system('cp {:s} {:s}'.format(self.file_paths[i], work_dir))
-                os.system('crest --constrain 1-4')
-                os.system('rm .xcon*')
 
-                f = open(os.path.join(work_dir, 'constraints.inp'), 'wt')
+                cwd = os.getcwd()
+
+                os.mkdir(work_dir)
+                os.system('cp {:s} {:s}'.format(self.file_paths[i], work_dir))
+
+                os.chdir(work_dir)
+
+                os.system('crest {:s} --constrain 1-4 >/dev/null 2>&1'.format(file_name))
+                os.remove('.xcontrol.sample')
+
+                f = open('constraints.inp', 'wt')
                 f.write(self.constraints[i])
                 f.close()
 
-                f = open(os.path.join(work_dir, '{:s}.sh'.format(label)), 'wt')
+                f = open('crest.sh', 'wt')
                 f.write(script.format(label=label, n_proc=self.n_proc, charge=self.charges[i], mult=self.mults[i]))
                 f.close()
+
+                os.chdir(cwd)
 
         return
 
@@ -82,19 +93,21 @@ class Crest():
         for i in range(n_struct):
             label = os.path.basename(self.file_paths[i]).rsplit('.', 1)[0]
             work_dir = os.path.join(self.prefix, label)
-            output = os.path.exists(os.path.join(work_dir, '{:s}.log'.format(label)))
+            output = os.path.join(work_dir, 'crest.log')
 
             if not os.path.exists(output):
                 if not dry_run:
                     cwd = os.getcwd()
                     os.chdir(work_dir)
-                    os.system('/bin/bash {:s}.sh'.format(label))
+                    os.system('/bin/bash crest.sh')
                     os.chdir(cwd)
 
             if os.path.exists(output):
                 status == check_normal_termination(output)
                 if status == False:
                     print(work_dir, 'Error termination')
+            else:
+                print(work_dir, 'No output')
 
         return
 
