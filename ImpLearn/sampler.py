8cfeb60252ab2ed37f641cdc47b58ff7397d1aff
changhae-kim <cakim2@bsharri.scs.illinois.edu>
2023-05-05 17:03:25 -0500

    fix build-up of redundant samples; add warning comment

--- ImpLearn/sampler.py
+++ ImpLearn/sampler.py
@@ -35,6 +35,12 @@ class Sampler():
             if random_state is not None:
                 self.rng.seed(random_state)
 
+            ###
+
+            # This piece of code might appear to contain multiple redundancies,
+            # but they are necessary to allow the exclusion of sites on the fly during an importance learning run.
+            # Take caution when editing.
+
             if replace:
                 candidates = [i for i in range(self.n_points)]
                 new_samples = list(self.rng.choice(candidates, size=batch_size, replace=True, p=weights))
@@ -52,7 +58,9 @@ class Sampler():
                 new_samples = self.sample(weights, batch_size=batch_size-len(samples))
                 samples.extend(new_samples)
 
-            self.samples.extend(samples)
+            self.samples.extend([i for i in samples if i not in self.samples])
+
+            ###
 
         else:
 
